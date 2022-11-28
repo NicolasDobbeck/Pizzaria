8761b26c7dd30887e859da4148f8af6e7f878fd6
Nicolas <nicolasdobbeckmendes12@gmail.com>
2022-11-28 11:01:15 -0300

    cards

--- js/section-promocao/card-promo.js
+++ js/section-promocao/card-promo.js
@@ -36,5 +36,77 @@ class card extends HTMLElement{
         this.shadow.appendChild(this.component())
         this.shadow.appendChild(this.styles())
     }
-    
+
+    styles(){
+        const style = document.createElement('style')
+        style.textContent =  
+        `
+            .card-pizza{
+                display: flex;
+                height: 350px;
+                width: 290px;
+                flex-direction: column;
+                margin-left: 90px;
+                gap: 5px;
+                /* border: 1px solid #000; */
+                border-radius: 10px;
+                box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+            }
+            
+            .card-pizza img{
+                display: flex;
+                border-radius: 5px 5px 0px 0px;
+            }
+            
+            .card-pizza h1{
+                color: #000;
+                margin-left: 10px;
+                font-family: 'Inter', sans-serif;
+                font-weight: 400;
+            }
+            
+            .card-descricao{
+                display: flex;
+                font-family: 'Inter', sans-serif;
+                margin-left: 10px;
+                font-weight: 300;
+            }
+            
+            .price{
+                display: flex;
+                justify-content: flex-end;
+                padding: 20px;
+                font-family: 'Inter', sans-serif;
+                font-size: 30px;
+                font-weight: 300
+            }
+            
+            .container-price{
+                display: flex;
+                background-color: #FF2727;
+                width: max-content;
+                border-radius: 5px;
+                margin-left: 10px;
+                margin-top: 5px;
+            }
+        `
+        return style
+    }
+
+    component(){
+        const card = document.createElement('div')
+        card.classList.add('card-pizza')
+        card.innerHTML = `
+        <img src="../assents/img/meat-pizza-fast-food-pizza-4k-baking.jpg" alt="">
+        <h1 class="card-titulo">Mussarela </h1>
+        <span class="card-descricao">Queijo e Tomate</span>
+        <div class="container-price">
+            <span class="price">R$30,99</span>
+        </div>
+        `
+        return card
+    }
 }
+
+//Para a web entender esse componente
+customElements.define('card-promocoes', card)
--- js/section-promocao/sectionPromo.js
+++ js/section-promocao/sectionPromo.js
@@ -1,1 +1,26 @@
 'use strict'
+
+import './card-promo.js'
+
+const fetchApiPizza = async function () {
+    const url = `` 
+    const response = await fetch(url);
+    const data = await response.json()
+
+    return data
+}
+
+const pizza = await fetchApiPizza();
+console.log(pizza)
+
+pizza.forEach(item => {
+    const container = document.querySelector('.container-cards')
+    const card = document.createElement('.card-promocoes')
+
+    card.setAttribute('nome', item.nome)
+    card.setAttribute('descricao', item.ingredientes)
+    card.setAttribute('imagem', item.image)
+    card.setAttribute('preco', item.preco)
+
+    container.appendChild(card)
+});
